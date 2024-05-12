9531796dbb1ee04b1dc09e8347cc1793b1814283
brocode <kelimbth@gmail.com>
2024-05-12 14:41:20 +0300

    first commit 🚀: details to be displayed on the onboarding screen

---
+++ lib/screens/onboarding/components/onboarding_items.dart
@@ -0,0 +1,19 @@
+class OnboardingScreenItems {
+  List<Map<String, String>> items = [
+    {
+      "text": "Welcome to soko, let's shop!",
+      "description": "",
+      "image": 'assets/img/shop.jpg',
+    },
+    {
+      "text": "Enjoy shopping!",
+      "description": "",
+      "image": 'assets/img/shop.jpg',
+    },
+    {
+      "text": "Here for you!",
+      "description": "",
+      "image": 'assets/img/shop.jpg',
+    },
+  ];
+}
