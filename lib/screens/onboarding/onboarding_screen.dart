ef583196f33009fa1db068affff4faf1a6a2c29b
brocode <kelimbth@gmail.com>
2024-05-12 14:35:37 +0300

    created an onboarding screen stateless widget

---
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -0,0 +1,14 @@
+import 'package:e_commerce/screens/onboarding/components/onboarding_body.dart';
+import 'package:flutter/material.dart';
+
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Body(),
+    );
+  }
+}
