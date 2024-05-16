89e9e0e4c018ae696aac4f9c1b816a10973c359a
brocode <kelimbth@gmail.com>
2024-05-16 15:19:11 +0300

    appended a new route name for forgot password

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/screens/forgot_password/forgot_password.dart';
 import 'package:e_commerce/screens/login/login.dart';
 import 'package:e_commerce/screens/onboarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
@@ -5,4 +6,5 @@ import 'package:flutter/material.dart';
 final Map<String, WidgetBuilder> routes = {
   OnboardingScreen.routeName: (context) => const OnboardingScreen(),
   LoginPage.routeName: (context) => const LoginPage(),
+  ForgotPasswordScreen.routeName:(context) => const ForgotPasswordScreen(),
 };
