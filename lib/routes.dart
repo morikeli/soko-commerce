269f16d4750154fd68d98ea739ffa12942f16c8f
brocode <kelimbth@gmail.com>
2024-05-17 16:50:59 +0300

    appended route name for signup screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,12 @@
 import 'package:e_commerce/screens/forgot_password/forgot_password.dart';
 import 'package:e_commerce/screens/login/login.dart';
 import 'package:e_commerce/screens/onboarding/onboarding_screen.dart';
+import 'package:e_commerce/screens/signup/signup.dart';
 import 'package:flutter/material.dart';
 
 final Map<String, WidgetBuilder> routes = {
   OnboardingScreen.routeName: (context) => const OnboardingScreen(),
   LoginPage.routeName: (context) => const LoginPage(),
   ForgotPasswordScreen.routeName:(context) => const ForgotPasswordScreen(),
+  SignupScreen.routeName: (context) => const SignupScreen(),
 };
