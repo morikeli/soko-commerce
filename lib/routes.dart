09cbfd329567fc1705c37620b4e5b90693e65eea
brocode <kelimbth@gmail.com>
2024-05-20 11:09:08 +0300

    appended route names for homepage and OTP screens

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,8 @@
 import 'package:e_commerce/screens/forgot_password/forgot_password.dart';
+import 'package:e_commerce/screens/homepage/homepage.dart';
 import 'package:e_commerce/screens/login/login.dart';
 import 'package:e_commerce/screens/onboarding/onboarding_screen.dart';
+import 'package:e_commerce/screens/otp/otp.dart';
 import 'package:e_commerce/screens/signup/signup.dart';
 import 'package:flutter/material.dart';
 
@@ -9,4 +11,6 @@ final Map<String, WidgetBuilder> routes = {
   LoginPage.routeName: (context) => const LoginPage(),
   ForgotPasswordScreen.routeName:(context) => const ForgotPasswordScreen(),
   SignupScreen.routeName: (context) => const SignupScreen(),
+  OTPScreen.routeName: (context) => const OTPScreen(),
+  Homepage.routeName: (context) => const Homepage(),
 };
