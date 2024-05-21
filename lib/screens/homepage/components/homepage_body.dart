d89ad658f8052392e3e0a02cf8b30f57f63ba757
brocode <kelimbth@gmail.com>
2024-05-21 13:32:06 +0300

    created widget with all components used for the homepage

---
+++ lib/screens/homepage/components/homepage_body.dart
@@ -0,0 +1,32 @@
+import 'package:e_commerce/screens/homepage/components/discount_banner.dart';
+import 'package:e_commerce/screens/homepage/components/homepage_header.dart';
+import 'package:e_commerce/screens/homepage/components/popular_products.dart';
+import 'package:e_commerce/screens/homepage/components/special_offers.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class HomepageBody extends StatelessWidget {
+  const HomepageBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height *.03),
+            const HomepageHeader(),
+            SizedBox(height: MediaQuery.of(context).size.height * .02),
+            const DiscountBanner(),
+            SizedBox(height: MediaQuery.of(context).size.height * .02),            
+            const SpecialOffersForYou(),
+            SizedBox(height: MediaQuery.of(context).size.height * .02),
+            const PopularProducts(),
+            SizedBox(height: MediaQuery.of(context).size.height * .02),
+          ],
+        ),
+      ),
+    );
+  }
+}
