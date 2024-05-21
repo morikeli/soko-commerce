ec7383a168cf95773fb10b94e915e25a72e8fbf9
brocode <kelimbth@gmail.com>
2024-05-21 13:36:54 +0300

    created a widget to display info of popular products

---
+++ lib/screens/homepage/components/product_card.dart
@@ -0,0 +1,68 @@
+import 'package:e_commerce/constants/colors.dart';
+import 'package:e_commerce/models/products.dart';
+import 'package:flutter/material.dart';
+
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key,
+    required this.product,
+  });
+  final Product product;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 20.0),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * .4,
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1.02,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: kSecondaryColor.withOpacity(.01),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(product.productImages[0]),
+              ),
+            ),
+            Text(
+              product.productName,
+              maxLines: 2,
+              style: const TextStyle(color: Colors.black),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: Text(
+                    '${product.productPrice}0/=',
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: kPrimaryColor
+                    ),
+                    
+                  ),
+                ),
+                InkWell(
+                  borderRadius: BorderRadius.circular(30.0),
+                  onTap: () {},
+                  child: Icon(
+                    Icons.favorite_outlined, 
+                    color: product.isFavorite ? Colors.red : kSecondaryColor.withOpacity(.01),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
