16bd233c022f4c5f6ee836c553c937d0b3e94b79
brocode <kelimbth@gmail.com>
2024-05-21 13:35:32 +0300

    made minor changes 1.) converted homepage widget to a stateful widget 2.) added bottom navigation bar

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -1,14 +1,35 @@
 import 'package:e_commerce/screens/homepage/components/homepage_body.dart';
+import 'package:e_commerce/screens/homepage/widgets/bottom_navbar.dart';
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   static String routeName = '/homepage';
   const Homepage({super.key});
 
   @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int _selectedScreen = 0;
+  final List<Widget> screens = [
+    const HomepageBody(),
+  ];
+
+  void _onIconTapped(int index) {
+    setState(() {
+      _selectedScreen = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomepageBody(),
+    return Scaffold(
+      body: const HomepageBody(),
+      bottomNavigationBar: BottomNavbar(
+        onTap: _onIconTapped,
+        screenIndex: _selectedScreen,
+      ),
     );
   }
 }
