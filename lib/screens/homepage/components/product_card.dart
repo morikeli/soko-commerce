13231584e0d3beccc85abd99f671222379281ebe
brocode <kelimbth@gmail.com>
2024-06-01 12:38:23 +0300

    updated code block 1.) added attributes in constructor - (lines 9 & 10) 2.) added attribute 'onPress' of type GestureTapCallBack 3.) wrapped SizedBox() with GestureDetector()

--- lib/screens/homepage/components/product_card.dart
+++ lib/screens/homepage/components/product_card.dart
@@ -7,60 +7,66 @@ class ProductCard extends StatelessWidget {
   const ProductCard({
     super.key,
     required this.product,
+    required this.onPress,
   });
+  
   final Product product;
+  final GestureTapCallback onPress;
   
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 20.0),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * .4,
-        child: Column(
-          children: [
-            AspectRatio(
-              aspectRatio: 1.02,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: kSecondaryColor.withOpacity(.01),
-                  borderRadius: BorderRadius.circular(15.0),
+      child: GestureDetector(
+        onTap: onPress,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * .4,
+          child: Column(
+            children: [
+              AspectRatio(
+                aspectRatio: 1.02,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: kSecondaryColor.withOpacity(.01),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(product.productImages[0]),
                 ),
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset(product.productImages[0]),
               ),
-            ),
-            Text(
-              product.productName,
-              maxLines: 2,
-              style: const TextStyle(color: Colors.black),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0),
-                  child: Text(
-                    '${product.productPrice}0/=',
-                    style: const TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: kPrimaryColor
+              Text(
+                product.productName,
+                maxLines: 2,
+                style: const TextStyle(color: Colors.black),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: Text(
+                      '${product.productPrice}0/=',
+                      style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: kPrimaryColor
+                      ),
+                      
                     ),
-                    
                   ),
-                ),
-                InkWell(
-                  borderRadius: BorderRadius.circular(30.0),
-                  onTap: () {},
-                  child: Icon(
-                    Icons.favorite_outlined, 
-                    color: product.isFavorite ? Colors.red : kSecondaryColor.withOpacity(.01),
+                  InkWell(
+                    borderRadius: BorderRadius.circular(30.0),
+                    onTap: () {},
+                    child: Icon(
+                      Icons.favorite_outlined, 
+                      color: product.isFavorite ? Colors.red : kSecondaryColor.withOpacity(.01),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
