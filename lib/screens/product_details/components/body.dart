bc2a2cdb068c71f11e84ee580eb710e2447e7971
brocode <kelimbth@gmail.com>
2024-06-04 10:53:23 +0300

    renamed modules - renamed dir. 'sections' to 'widgets'

--- lib/screens/product_details/components/body.dart
+++ lib/screens/product_details/components/body.dart
@@ -1,6 +1,6 @@
 import 'package:e_commerce/models/products.dart';
-import 'package:e_commerce/screens/product_details/components/sections/add_to_cart.dart';
-import 'package:e_commerce/screens/product_details/components/sections/product_description.dart';
+import 'package:e_commerce/screens/product_details/components/widgets/add_to_cart.dart';
+import 'package:e_commerce/screens/product_details/components/widgets/product_description.dart';
 import 'package:flutter/material.dart';
 
 
