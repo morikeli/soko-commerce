6a0fb597f730cc2b309de24c33ada75873ac2a04
brocode <kelimbth@gmail.com>
2024-06-05 10:35:31 +0300

    added description in onboarding screen

--- lib/screens/onboarding/components/onboarding_items.dart
+++ lib/screens/onboarding/components/onboarding_items.dart
@@ -2,17 +2,17 @@ class OnboardingScreenItems {
   List<Map<String, String>> items = [
     {
       "text": "Welcome to soko, let's shop!",
-      "description": "",
+      "description": "Soko is your favorite electronics online store. Checkout available products for sale.  Don't miss out on special offers.",
       "image": 'assets/img/shop.jpg',
     },
     {
       "text": "Enjoy shopping!",
-      "description": "",
+      "description": "We have a variety of electronics available and wide range of brands to choose from. Buy original electronics from us.",
       "image": 'assets/img/shop.jpg',
     },
     {
       "text": "Here for you!",
-      "description": "",
+      "description": "Enjoy our services and buy your electronics at affordable prices today.",
       "image": 'assets/img/shop.jpg',
     },
   ];
